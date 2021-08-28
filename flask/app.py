028afea2f2416dae55425dfd009e95b04e921dd4
Andy Shen <andyshenproper@gmail.com>
2021-08-28 12:50:21 -0700

    remove ICD credentials

--- flask/app.py
+++ flask/app.py
@@ -18,21 +18,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
-    query = "";
     if request.method == 'POST':
         query = request.form['search']
 
-        print(search(query).text)
-        # r_dict = json.loads(search(query).text)
-        # for entity in r_dict['destinationEntities']:
-        #     print(entity)
-        #     print(entity['id'])
-        #     id_uri = entity['id']
-        #     tokens = id_uri.split("/")
-        #     token = tokens[5]
-        #     print(token)
-        #     break
-    return render_template("quiz.html", query=query)
+        r_dict = json.loads(search(query).text)
+        for entity in r_dict['destinationEntities']:
+            id_uri = entity['id']
+            items = id_uri.split("/")
+            id = items[5]
+            print(id)
+            name = entity['title']
+            break
+    return render_template("quiz.html")
 
 def getToken():
     r = requests.post(token_endpoint, data=payload, verify=False).json();
@@ -43,7 +40,7 @@ def search(query):
     token = getToken()
     useFlexisearch = 'true'
     headers = {
-        "Authorization": "Bearer" + token,
+        "Authorization": "Bearer " + token,
         "Accept": "application/json",
         "Accept-Language": "en",
         "API-Version": "v2"
--- flask/credentials.py
+++
@@ -1,2 +0,0 @@
-client_id = "ad011ba7-1e96-4932-9d40-c1f28054e8fa_d0996b9f-08ef-4b74-a8a2-565d2550d266"
-client_secret = "F7nKrGPgfcGNQ7en2CBKZJhd4Pi4LP7F8wbe7nLUXqc="
