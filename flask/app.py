1c027d91f9a27bd50a2ecb768fb70287f54c9d59
Andy Shen <andyshen@dhcp-128-189-226-254.ubcsecure.wireless.ubc.ca>
2021-10-14 12:12:45 -0700

    Fix file uploading scheme, rework confirmation page

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,9 @@
-import os
+from os.path import join
 from flask import Flask, flash, request, render_template, url_for, session
 from werkzeug.utils import redirect, secure_filename
 import icdutils
 
-UPLOAD_FOLDER = "/static/img"
+UPLOAD_FOLDER = "./static/img/"
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'bmp', 'pdf', 'tiff', 'gif'}
 
 app = Flask(__name__)
@@ -18,6 +18,7 @@ def home():
 
 @app.route('/quiz', methods=['POST', 'GET'])
 def quiz():
+    back_url = url_for('quiz')
     if request.method == 'POST':
         query = request.form['search']
         IDs = icdutils.searchGetIDs(query)
@@ -25,35 +26,47 @@ def quiz():
         confirmation = "Incorrect"
         if confirmID == '1627987797':
             confirmation = "Correct!"
-        return redirect(url_for('confirm', confirmation=confirmation))
+        return redirect(url_for('confirm', confirmation=confirmation, back_url=back_url))
     return render_template("quiz.html")
 
 @app.route('/submit', methods=['POST', 'GET'])
 def submit():
+    back_url = url_for('submit')
     results=[]
     if request.method == 'POST':
         if request.form['results'] != "Search diagnosis":
-            if 'file' not in request.files:
+            # check for no file
+            if 'filename' not in request.files:
                 return redirect(request.url);
-            file = request.files['file']
+            
+            file = request.files['filename']
+            
+            # check for empty file name
             if file.filename == '':
                 return redirect(request.url)
+            
+            # file exists
             if file and allowedFile(file.filename):
+                print("## EMPTY FILENAME ##")
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                return redirect(url_for(request.url))
+                print(join(app.config['UPLOAD_FOLDER']))
+                file.save(join(app.config['UPLOAD_FOLDER'], filename))
+                confirmation = "Uploaded successfully!"
+                return redirect(url_for('confirm', confirmation=confirmation, back_url=back_url))
         else:
             query = request.form['search']
+            if query == "":
+                return redirect(request.url)
             results = icdutils.searchGetPairs(query)
     return render_template("submit.html", results=results)
 
 @app.route('/confirm', methods=['POST', 'GET'])
 def confirm():
     confirmation = request.args['confirmation']
+    back_url = request.args['back_url']
     if request.method == 'POST':
-        print("submitted?")
-        return redirect(url_for('quiz'))
-    return render_template("confirmation.html", confirmation=confirmation)
+        return redirect(back_url)
+    return render_template("confirmation.html", confirmation=confirmation, back_url=back_url)
 
 @app.route('/verify')
 def verify():
