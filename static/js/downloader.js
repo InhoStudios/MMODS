3126687ccab45983745f943494ab0c6f77417639
Andy Shen <andyshenproper@gmail.com>
2022-03-30 21:41:08 -0700

    Add downloader template to webpage

---
+++ static/js/downloader.js
@@ -0,0 +1,29 @@
+var checkboxes = document.querySelectorAll("input[type=checkbox][name=imgselect]");
+let enabledSettings = [];
+
+var downloadBar = document.getElementById("downloadbar");
+var downloadBarText = document.getElementById("downloadbartxt");
+
+checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+        enabledSettings = 
+            Array.from(checkboxes) // Convert checkboxes to an array to use filter and map.
+            .filter(i => i.checked) // Use Array.filter to remove unchecked checkboxes.
+            .map(i => i.value) // Use Array.map to extract only the checkbox values from the array of objects.
+        if (enabledSettings.length != 0) {
+            if (downloadBar.classList.contains("hidden-passthrough")) {
+                downloadBar.classList.remove("hidden-passthrough");
+            }
+            var num = enabledSettings.length;
+            var downloadConfirmation = "Download " + num + " image";
+            if (num > 1) downloadConfirmation = downloadConfirmation + "s";
+            downloadConfirmation = downloadConfirmation + "?";
+            downloadBarText.innerText = downloadConfirmation;
+            console.log(enabledSettings);
+        } else {
+            if (!downloadBar.classList.contains("hidden-passthrough")) {
+                downloadBar.classList.add("hidden-passthrough");
+            }
+        }
+    })
+});
