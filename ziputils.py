a07d012f1d93f252ed4f312a08f40489f7adb236
Andy Shen <andyshenproper@gmail.com>
2022-06-24 18:32:12 -0700

    Add rough docstrings to ziputils

--- ziputils.py
+++ ziputils.py
@@ -3,9 +3,22 @@ from io import BytesIO
 
 class InMemoryZip(object):
     def __init__(self):
+        """
+        Create an InMemoryZip Object
+        """
         self.in_memory_zip = BytesIO()
     
     def add_file(self, filename, contents):
+        """
+        Adds an ephemeral file to the zip archive.
+
+        Parameters
+        -----
+        filename: str
+            A string corresponding to the name of the file in the zip archive
+        contents: str
+            A string that holds all the contents to be written to the file
+        """
         zf = ZipFile(self.in_memory_zip, "a", ZIP_DEFLATED, False)
 
         zf.writestr(filename, contents)
@@ -16,6 +29,14 @@ class InMemoryZip(object):
         return self
     
     def add_image(self, imagepath):
+        """
+        Adds an image by filepath to the zip archive.
+
+        Parameters
+        -----
+        imagepath: str
+            The path to the image to be written into the zip archive.
+        """
         zf = ZipFile(self.in_memory_zip, "a", ZIP_DEFLATED, False)
 
         zf.write(imagepath)
@@ -25,10 +46,25 @@ class InMemoryZip(object):
         return self
 
     def read(self):
+        """
+        Reads the data from the zip archive.
+
+        Returns
+        -----
+        in_memory_zip.read(): BytesIO()
+            Representation of the zip archive as a stream of bytes
+        """
         self.in_memory_zip.seek(0)
         return self.in_memory_zip.read()
 
     def to_byte_stream(self):
+        """
+        Converts zip archive to stream of bytes to be sent to users as a file.
+
+        Returns:
+        zfb: BytesIO()
+            A BytesIO representation of the zip file.
+        """
         zfb = BytesIO()
         zfb.write(self.read())
         zfb.seek(0)
