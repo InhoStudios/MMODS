5662522db704cbfb67a36c71a04962ebb6e36d06
Andy Zhao <ndsz@students.cs.ubc.ca>
2023-05-08 12:24:48 -0700

    Begin working on front end

---
+++ ilds-gallery/src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
+
+function Layout() {
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+        </>
+    );
+}
--- ilds-gallery/src/components/Navbar.jsx
+++ ilds-gallery/src/components/Navbar.jsx
@@ -4,27 +4,27 @@ import { Link } from "react-router-dom";
 class Navbar extends React.Component {
     render() {
         return (
-            <div class="container-fluid" style="width:100%">
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="navbar-brand text-left" href="/">ICD-Database</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <div className="container-fluid" style="width:100%">
+                <nav className="navbar navbar-expand-lg navbar-light">
+                    <a className="navbar-brand text-left" href="/">ICD-Database</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class=" text-right">
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="/">Home</a>
+                    <div className=" text-right">
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/">Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/quiz">Quiz</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/quiz">Quiz</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/submit">Submit</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/submit">Submit</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/verify">Verify</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/verify">Verify</a>
                             </li>
                             </ul>
                         </div>
