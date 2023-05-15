6dcdf6039de35606b76a877a86cadb2a263385d7
Andy Zhao <ndsz@students.cs.ubc.ca>
2023-05-15 16:21:09 -0700

    Remove credentials from git file structure

--- credentials.py
+++
@@ -1,4 +0,0 @@
-client_id = "ad011ba7-1e96-4932-9d40-c1f28054e8fa_255040ac-2a41-46f3-95b2-9539a6a2b10c"
-client_secret = "w4O0kGQ/4rv8OhvwyafVRAOiqm4Y6wv/HxTwyTJl0Oo="
-sql_user = "root"
-sql_password = "wasd8064.MSL"
