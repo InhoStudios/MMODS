4d09c08458a9fca881f0b0acac0f7fe6ad6d8f89
Andy Zhao <ndsz@students.cs.ubc.ca>
2023-05-17 16:09:19 -0700

    Add SQL backend to express middleware

---
+++ server/routes/sqldb.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var sql = require('../utilities/SQLManager')
+
+router.get('/', async (req, res, next) => {
+    let data = await sql.postQuery("SELECT 1 + 1 AS solution WHERE true");
+    console.log(data);
+    res.send(data);
+});
+
+module.exports = router;
