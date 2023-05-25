a0f3a5f5576a0988142b0dcc88516b66fc308b13
Andy Zhao <ndsz@students.cs.ubc.ca>
2023-05-25 09:12:04 -0700

    Remove credentials file

--- flask/credentials.py
+++
@@ -1,4 +0,0 @@
-client_id = "ad011ba7-1e96-4932-9d40-c1f28054e8fa_5c67efb4-7f29-4ede-98d3-70d7d284394a"
-client_secret = "3h7YkNsIHTtsMi9P3qwHzy4EoYkWfVKLltbw1alVbe0="
-sql_user = "root"
-sql_password = "wasd8064.MSL"
