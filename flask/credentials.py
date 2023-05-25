9dc02f0bca317204db358ce309ec17f1ce67980d
Andy Zhao <ndsz@students.cs.ubc.ca>
2023-05-25 09:11:17 -0700

    Clean file structure

--- flask/credentials.py
+++ flask/credentials.py
@@ -1,2 +1,4 @@
-client_id = "ad011ba7-1e96-4932-9d40-c1f28054e8fa_d0996b9f-08ef-4b74-a8a2-565d2550d266"
-client_secret = "F7nKrGPgfcGNQ7en2CBKZJhd4Pi4LP7F8wbe7nLUXqc="
+client_id = "ad011ba7-1e96-4932-9d40-c1f28054e8fa_5c67efb4-7f29-4ede-98d3-70d7d284394a"
+client_secret = "3h7YkNsIHTtsMi9P3qwHzy4EoYkWfVKLltbw1alVbe0="
+sql_user = "root"
+sql_password = "wasd8064.MSL"
