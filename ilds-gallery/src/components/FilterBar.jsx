40b4aaae099a91c32133fcd6f248646ad9d1fd28
Andy Zhao <ndsz@students.cs.ubc.ca>
2023-06-01 08:59:01 -0700

    Add categorization and flexibilty for IP changes

---
+++ ilds-gallery/src/components/FilterBar.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { SERVER_ENDPOINT } from "../utilities/Structures";
+
+export default class FilterBar extends React.Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+            image_types: [],
+            icd_entities: [],
+        };
+        this.getEntitiesFromDB();
+    }
+
+    async getEntitiesFromDB() {
+        let entities = await fetch(`${SERVER_ENDPOINT}/db_select?values=distinct e.entity_title, e.entity_id&from=ICD_Entity e, Cases c&where=c.user_selected_entity=e.entity_id`)
+            .then((data) => data.json())
+            .catch((err) => console.log(err));
+        this.setState({icd_entities: entities});
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col-lg-8">
+                    <div className="dropdown px-2">
+                        <button className="btn btn-outline-secondary">Severity ↓</button>
+                        <div className="dropdown-content">
+                            <a className="px-2">
+                                <label>
+                                    <input type="checkbox" className="mx-2" value="All"></input>
+                                    All
+                                </label>
+                            </a>
+                            <a className="px-2">
+                                <label>
+                                    <input type="checkbox" className="mx-2" value="Benign"></input>
+                                    Benign
+                                </label>
+                            </a>
+                            <a className="px-2">
+                                <label>
+                                    <input type="checkbox" className="mx-2" value="Malignant"></input>
+                                    Malignant
+                                </label>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="dropdown px-2">
+                        <button className="btn btn-outline-secondary">Image Type ↓</button>
+                        <div className="dropdown-content">
+                            <a className="px-2">
+                                <label>
+                                    <input type="checkbox" className="mx-2" value="Clinical"></input>
+                                    Clinical
+                                </label>
+                            </a>
+                            <a className="px-2">
+                                <label>
+                                    <input type="checkbox" className="mx-2" value="Clinical"></input>
+                                    Dermoscopy
+                                </label>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="dropdown px-2">
+                        <button className="btn btn-outline-secondary">ICD Entity Classification ↓</button>
+                        <div className="dropdown-content">
+                            <a className="px-2">
+                                <label>
+                                    <input type="checkbox" className="mx-2" value="Clinical"></input>
+                                    All diagnoses
+                                </label>
+                            </a>
+                            {
+                                this.state.icd_entities.map((entity) => (
+                                <a className="px-2">
+                                    <label>
+                                        <input type="checkbox" className="mx-2" value="Clinical"></input>
+                                        {entity.entity_title}
+                                    </label>
+                                </a>
+                                ))
+                            }
+                            {/*{% for key in categories.keys() %}*/}
+                            {/*<a href="#{{ key }}">{{categories[key]}}</a>*/}
+                            {/*{% endfor %}*/}
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-4">
+                    <form method="post">
+                        <input type="input" className="form-control form-control-lg" placeholder="Search" />
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
--- ilds-gallery/src/pages/Home.jsx
+++ ilds-gallery/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ImageCard from "../components/ImageCard";
 import { SERVER_ENDPOINT } from "../utilities/Structures";
+import FilterBar from "../components/FilterBar";
 
 export default class Home extends React.Component {
     constructor(props) {
@@ -17,6 +18,7 @@ export default class Home extends React.Component {
             .catch((err) => console.log(err));
         this.setState({images: images});
     }
+
     render() {
 
         return (
@@ -26,10 +28,10 @@ export default class Home extends React.Component {
                         <div className="col-md-4 mb-2 text-left">
                             <div className="row justify-content-left mt-5 mb-3 text-left">
                                 <h1>ILDS Gallery</h1>
-                                <p>[description]</p>
+                                <p>Research and educational database of dermatology images. Hosted privately by @InhoStudios.</p>
                             </div>
                             <div className="row mt-3">
-                                <a className="btn" href="/quiz">
+                                {/* <a className="btn" href="/quiz">
                                     <div className="card card-shadow text-left">
                                         <div className="card-body">
                                             <span>/quiz</span>
@@ -37,7 +39,7 @@ export default class Home extends React.Component {
                                             <p>Take a dermatology quiz!</p>
                                         </div>
                                     </div>
-                                </a>
+                                </a> */}
                                 <a className="btn" href="/submit">
                                     <div className="card card-shadow text-left">
                                         <div className="card-body">
@@ -58,20 +60,21 @@ export default class Home extends React.Component {
                                 </a>
                             </div>
                         </div>
-                        <div className="col-md-8 mb-2 text-left">
+                        <div className="col-lg-8 mb-2 text-left">
                             <div className="row justify-content-center mt-5 text-center">
                                 <h3>Verified Images</h3>
-                                <div className="row mt-2">
-                                    {
-                                        this.state.images.map((image) => (
-                                            <ImageCard
-                                                filepath={image.url}
-                                                uri={image.user_selected_entity}
-                                                title={image.entity_title}
-                                                colsize={"col-lg-4"}/>
-                                        ))
-                                    }
-                                </div>
+                            </div>
+                            <FilterBar />
+                            <div className="row mt-2">
+                                {
+                                    this.state.images.map((image) => (
+                                        <ImageCard
+                                            filepath={image.url}
+                                            uri={image.user_selected_entity}
+                                            title={image.entity_title}
+                                            colsize={"col-lg-4"}/>
+                                    ))
+                                }
                             </div>
                         </div>
                     </div>
